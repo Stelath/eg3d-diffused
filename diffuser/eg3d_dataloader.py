b82049852dc95f6802f08b98e4549e7690a8a1dd
Stelath <korte@clemson.edu>
2022-12-13 00:47:13 -0500

    Updated Dataset

--- diffuser/eg3d_dataloader.py
+++ diffuser/eg3d_dataloader.py
@@ -6,9 +6,9 @@ import torch
 from torch.utils.data import Dataset
 
 class EG3DDataDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.eg3d_data = pd.read_csv(csv_file)
-        self.root_dir = root_dir
+    def __init__(self, csv_file, data_dir, transform=None):
+        self.eg3d_data = pd.read_csv(os.path.join(data_dir, csv_file))
+        self.data_dir = data_dir
         self.transform = transform
         
     def __len__(self):
@@ -18,12 +18,12 @@ class EG3DDataDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
             
-        img_name = os.path.join(self.root_dir, self.eg3d_data.iloc[idx, 0])
-        image = io.imread(img_name)
-        label = self.eg3d_data.iloc[idx, 1]
-        sample = {'image': image, 'label': label}
+        img_path = os.path.join(self.data_dir, self.eg3d_data.iloc[idx, 0])
+        image = io.imread(img_path)
+        encoded_vector = self.eg3d_data.iloc[idx, 1]
+        item = {'image': image, 'encoded_vector': encoded_vector}
         
         if self.transform:
-            sample = self.transform(sample)
+            item = self.transform(item)
             
-        return sample
+        return item
