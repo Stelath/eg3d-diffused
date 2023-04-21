7c6334e4dff62f4db440c0841e76f215e0c6501e
Stelath <korte@clemson.edu>
2023-04-21 21:48:10 +0000

    Install Script for PIP on new TPU VMs

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install lightning
+pip install 'transformers[torch]'
+pip install diffusers["torch"]
+pip install pandas
+pip install starlette==0.22.0 
+pip install taming-transformers
+pip install tensorboard
